fixed thread calculation error; working on command line arg parse

Analyze.py
@@ -24,8 +24,6 @@ def ArrayToGraph(fileName):
 
     edge_edge_array = np.array(edge_edge_array[0])
 
-    print(num_nodes)
-
     # Check if the size of edge_edge_array is a multiple of 2
     if edge_edge_array.size % 2 != 0:
         raise ValueError("Size of edge_edge_array must be a multiple of 2")
@@ -43,9 +41,9 @@ def ArrayToGraph(fileName):
     G.add_edges_from(edge_array)
 
     # Print the number of nodes and the edges of the graph
-    print("Number of nodes:", num_nodes)
-    print("Edges:")
-    print(G.edges())
+    # print("Number of nodes:", num_nodes)
+    # print("Edges:")
+    # print(G.edges())
     return G
 
 
@@ -61,10 +59,26 @@ def group_elements(arr):
     return result
 
 def read_double_values(file_path):
-    with open(file_path, 'r') as file:
-    # Read all lines from the file, strip trailing whitespace, 
-    # convert to float, and add to a list
-        return [float(line.strip()) for line in file]
+    arr = []
+    try:
+        with open(file_path, 'r') as file:
+            for line in file:
+                try:
+                    value = float(line.strip())
+                    arr.append(value)
+                except ValueError:
+                    print(f"Invalid value found: {line.strip()}")
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+    except IOError:
+        print(f"Error reading file: {file_path}")
+
+    if len(arr) == 0:
+        print("No valid values found in the file.")
+
+    return arr
+
+        
 
 def calculate_edge_crossings(graph, pos):
     crossings = 0
@@ -122,30 +136,32 @@ def do_edges_cross(p1, p2, p3, p4):
 # fr_time = end_time_fr - start_time_fr
 # normal_pos = group_elements(normal_pos)
 
-arr = read_double_values("position_data/fr_CUDA.txt")
-cuda_pos = group_elements(arr)
-
-arr = read_double_values("position_data/fr_CPU.txt")
-normal_pos = group_elements(arr)
-
-G = ArrayToGraph('graph_data/random.txt')
-
-print(len(G.nodes()))
 
 
-if ENABLE_CROSSING_EDGES:
-    print("edge crossings (fr_cuda): " + str(calculate_edge_crossings(G, cuda_pos)))
+if __name__ == "__main__":
 
-if ENABLE_CROSSING_EDGES:
-    print("edge crossings (fr): " + str(calculate_edge_crossings(G, normal_pos)))
+    arr = read_double_values("position_data/fr_CUDA.txt")
+    cuda_pos = group_elements(arr)
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+    arr = read_double_values("position_data/fr_CPU.txt")
+    normal_pos = group_elements(arr)
 
-ax1.set_title('Graph 1 (fr_cuda)\nTime: {:.4f} seconds'.format(1.1))
-nx.draw(G, pos=cuda_pos, ax=ax1)
+    G = ArrayToGraph('graph_data/random.txt')
 
-ax2.set_title('Graph 2 (fr)\nTime: {:.4f} seconds'.format(1.1))
-nx.draw(G, pos=normal_pos, ax=ax2)
 
-plt.tight_layout()
-plt.show()
+    if ENABLE_CROSSING_EDGES:
+        print("edge crossings (fr_cuda): " + str(calculate_edge_crossings(G, cuda_pos)))
+
+    if ENABLE_CROSSING_EDGES:
+        print("edge crossings (fr): " + str(calculate_edge_crossings(G, normal_pos)))
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+
+    ax1.set_title('Graph 1 (fr_cuda)\nTime: {:.4f} seconds'.format(1.1))
+    nx.draw(G, pos=cuda_pos, ax=ax1)
+
+    ax2.set_title('Graph 2 (fr)\nTime: {:.4f} seconds'.format(1.1))
+    nx.draw(G, pos=normal_pos, ax=ax2)
+
+    plt.tight_layout()
+    plt.show()

GenData.py
@@ -13,31 +13,59 @@ def serialize_array(arr, filename, aorw):
             f.write("%d " % item)
         f.write("\n")  # Add a newline character at the end
 
-
-
 # convert the 2d array G.edges, structured as:
     # [[v1, v2],[v1,v3],[v4,v5],...[v45, v48]]
     # to: [v1, v2, v1, v3...v45, v48]
 def GraphToArray(G, fileName):
-    G_dict = {node: list(G.neighbors(node)) for node in G.nodes()}
-    edge_edge_array = np.array(list(G.edges)).flatten()
+    # G_dict = {node: list(G.neighbors(node)) for node in G.nodes()}
+    # edge_edge_array = np.array(list(G.edges)).flatten()
+    edges = list(G.edges)
+    edge_edge_array = [node for edge in edges for node in edge]
     serialize_array(edge_edge_array, fileName, 'w')
 
     # Append the number of nodes
     num_nodes = len(G.nodes())
     serialize_array(np.array([num_nodes]), fileName, 'a')
 
+def generate_grid_graph(rows, columns):
+    G = nx.Graph()
+    
+    # Add nodes
+    for r in range(rows):
+        for c in range(columns):
+            node_id = r*rows+c
+            G.add_node(node_id)
+    
+    # Add edges
 
-NUMNODES = 500
-G = nx.gnp_random_graph(NUMNODES, 0.05, 42)
-GraphToArray(G, "graph_data/random.txt")
+    for r in range(rows):
+        for c in range(columns):
+            node_id = r*rows+c
+            
+            # Connect horizontally
+            if c < columns - 1:
+                neighbor_id = r*rows+(c+1)
+                G.add_edge(node_id, neighbor_id)
+            
+            # Connect vertically
+            if r < rows - 1:
+                neighbor_id = (r+1)*rows+c
+                G.add_edge(node_id, neighbor_id)
+    
+    return G
 
-# Generate the grid graph
-m = 50  # Number of rows
-n = 50  # Number of columns
-G = nx.grid_2d_graph(m, n)
-GraphToArray(G, "graph_data/grid.txt")
+if __name__ == "__main__":
 
-# G_dict = {node: list(G.neighbors(node)) for node in G.nodes()}
-# edge_edge_array = np.array(list(G.edges)).flatten()
-# serialize_array(edge_edge_array, "random_50.txt")
+    NUMNODES = 500
+    G = nx.gnp_random_graph(NUMNODES, 0.05, 42)
+    GraphToArray(G, "graph_data/random.txt")
+
+    # Generate the grid graph
+    m = 50  # Number of rows
+    n = 50  # Number of columns
+    G = nx.grid_2d_graph(m, n)
+    GraphToArray(G, "graph_data/grid.txt")
+
+    # G_dict = {node: list(G.neighbors(node)) for node in G.nodes()}
+    # edge_edge_array = np.array(list(G.edges)).flatten()
+    # serialize_array(edge_edge_array, "random_50.txt")

test.py
@@ -1,12 +1,54 @@
+# common libraries
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import time
 import subprocess
+import math
 
-# Call the compiled C++ program
-result = subprocess.run(["./x64/Debug/CudaRuntime2.exe"], capture_output=True, text=True)
 
-# Check the return code
-if result.returncode == 0:
-    # Print the program output
-    print("Program output:\n", result.stdout)
-else:
-    # Print the error message
-    print("An error occurred:\n", result.stderr)
+def generate_grid_graph(rows, columns):
+    G = nx.Graph()
+    
+    # Add nodes
+    for r in range(rows):
+        for c in range(columns):
+            node_id = r*rows+c
+            G.add_node(node_id)
+    
+    # Add edges
+
+    for r in range(rows):
+        for c in range(columns):
+            node_id = r*rows+c
+            
+            # Connect horizontally
+            if c < columns - 1:
+                neighbor_id = r*rows+(c+1)
+                G.add_edge(node_id, neighbor_id)
+            
+            # Connect vertically
+            if r < rows - 1:
+                neighbor_id = (r+1)*rows+c
+                G.add_edge(node_id, neighbor_id)
+    
+    return G
+
+
+ENABLE_CROSSING_EDGES = False
+
+NUMNODES = 5
+# G = nx.gnp_random_graph(NUMNODES, 0.05, 42)
+# GenData.GraphToArray(G, "graph_data/random.txt")
+
+# Generate the grid graph
+
+G = generate_grid_graph(5, 6)
+# edges = list(G.edges)
+# edge_edge_array = [node for edge in edges for node in edge]
+
+# print(edge_edge_array)
+
+G_dict = {node: list(G.neighbors(node)) for node in G.nodes()}
+edge_edge_array = np.array(list(G.edges)).flatten()
+print(edge_edge_array)